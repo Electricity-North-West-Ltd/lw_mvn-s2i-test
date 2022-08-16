ef4295f793ed0a474474a0545a5d13898de98f42
N386942 <leon.wild@enwl.co.uk>
2022-08-16 11:44:49 +0100

    Check other header attributes

--- src/main/java/com/enwl/assets/controller/TestController.java
+++ src/main/java/com/enwl/assets/controller/TestController.java
@@ -9,10 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @RestController
 public class TestController {
+
+    private final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
     @GetMapping("/test")
     public String test(HttpServletRequest request){
-        var headerIp = request.getHeader("X-FORWARDED-FOR");
-        var forwardedForStr = (headerIp == null)?"":", X-FORWARDED-FOR: "+headerIp;
-        return "Test controller called from "+request.getRemoteAddr()+forwardedForStr;
+        var headerAttributes = "";
+        for (var header: IP_HEADER_CANDIDATES) {
+            var ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                var ip = ipList.split(",")[0];
+                headerAttributes+=", "+header+": "+ip;
+            }
+        }
+        return "Test controller called from "+request.getRemoteAddr()+headerAttributes;
     }
 }
