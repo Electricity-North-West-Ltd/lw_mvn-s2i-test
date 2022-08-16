b934daefcb58346551aaa0aef03043499b4194c4
N386942 <leon.wild@enwl.co.uk>
2022-08-16 10:55:40 +0100

    Add client IP into controller reply

--- src/main/java/com/enwl/assets/controller/TestController.java
+++ src/main/java/com/enwl/assets/controller/TestController.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 public class TestController {
     @GetMapping("/test")
-    public String test(){
-        return "Test controller";
+    public String test(HttpServletRequest request){
+        return "Test controller called from "+request.getRemoteAddr();
     }
 }
